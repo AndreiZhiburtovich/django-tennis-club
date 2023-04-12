17ca925aa1fc73df665f292f450275ae4309ec81
Andrei Zhiburtovich <andzib@gmail.com>
2023-04-12 22:08:38 +0300

    Updated members / models.py, urls.py, views.py.

--- members/views.py
+++ members/views.py
@@ -2,10 +2,25 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Member
 
+
 def members(request):
-  mymembers = Member.objects.all().values()
-  template = loader.get_template('all_members.html')
-  context = {
-    'mymembers': mymembers,
-  }
-  return HttpResponse(template.render(context, request))
+    my_members = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'my_members': my_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    my_member = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'my_member': my_member,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
